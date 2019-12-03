d512f4c9d2527519883ecc71b4c691241e0a0cb3
shecks <shecks@gmail.com>
2019-12-03 20:06:44 +0000

    Application skeleton code

--- app/src/main/java/ie/justonetech/roadtriptracker/ui/activities/MainActivity.kt
+++ app/src/main/java/ie/justonetech/roadtriptracker/ui/activities/MainActivity.kt
@@ -2,12 +2,27 @@ package ie.justonetech.roadtriptracker.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import ie.justonetech.roadtriptracker.R
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContentView(R.layout.main_activity)
+
+        navHostFragment.findNavController().also { navController ->
+            AppBarConfiguration.Builder(navController.graph).build().also {
+                setSupportActionBar(toolBar)
+                setupActionBarWithNavController(navController, it)
+            }
+
+            bottomNavigationView.setupWithNavController(navController)
+        }
     }
 }
---
+++ app/src/main/java/ie/justonetech/roadtriptracker/ui/fragments/HistoryFragment.kt
@@ -0,0 +1,15 @@
+package ie.justonetech.roadtriptracker.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ie.justonetech.roadtriptracker.R
+
+class HistoryFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.history_fragment, container, false)
+    }
+}
